64eacc143e065e8292edf4c26c4b5339518cdb0e
Saloni0404 <you@example.com>
2026-05-26 23:17:57 -0700

    music updatev11

--- src/inviteConfig.js
+++ src/inviteConfig.js
@@ -5,8 +5,8 @@ export const invite = {
   /*intro: 'We would like to invite you to the engagement ceremony of',*/
   dateLabel: 'Sunday, June 14, 2026',
   timeLabel: '7:00am IST onwards',
-  /*locationShort: 'Shubham Halls, Sunnyvale, CA',*/
-  /*address: '1208 Apollo Way Suite 500, Sunnyvale, CA 94085',*/
+  locationShort: 'Shubham Halls, Sunnyvale, CA',
+  address: '1208 Apollo Way Suite 500, Sunnyvale, CA 94085',
   airportLine: '6.5 miles from San José Mineta Airport (SJC) and 31 miles from San Francisco International Airport (SFO)',
   mapLink: 'https://www.google.com/maps/search/?api=1&query=1208%20Apollo%20Way%20Suite%20500%2C%20Sunnyvale%2C%20CA%2094085',
  /* rsvpLink: 'https://forms.gle/REPLACE_WITH_YOUR_RSVP_FORM',*/
